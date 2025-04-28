2b4fe7e559d9046c0b40114ddbaa8ebc451263bc
Ajax Dong <i@aajax.top>
2025-04-28 18:00:37 +0800

    Network measuring (#63)
    * Adjusted dark mode mermaid graph theme
    
    * Network Mapping
    
    * Switch to online-rendered mermaid

---
+++ filter/hexo-filter-mermaid/index.js
@@ -0,0 +1,26 @@
+hexo.extend.tag.register("mermaid", (args, diagram) => {
+    const conf = hexo.config.mermaid || {
+        darkModeEnabled: false,
+        init: [""]
+    };
+
+    if (conf.darkModeEnabled) {
+        return `<pre class="mermaid mermaid-light">${conf.init[0]}\n${diagram}</pre>
+        <pre class="mermaid mermaid-dark">${conf.init[1]}\n${diagram}</pre>`;
+    }
+
+    return `<pre class="mermaid">${conf.init[0]}\n${diagram}</pre>`;
+}, {
+    async: true,
+    ends: true
+});
+
+hexo.extend.filter.register('before_post_render', (data) => {
+    if ('.md'.indexOf(data.source.substring(data.source.lastIndexOf('.')).toLowerCase()) > -1) {
+        data.content = data.content
+            .replace(/(\s*)(```) *(mermaid) *\n?([\s\S]+?)\s*(\2)(\n+|$)/g, (raw, start, startQuote, lang, content, endQuote, end) => {
+                // replace with an async call
+                return start + '{% ' + lang + ' %}' + content + '{% end' + lang + ' %}' + end;
+            });
+    }
+}, 0);
