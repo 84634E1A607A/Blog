134a49c5f339c687522d5b8ebd33836a737e06ae
Ajax Dong <i@aajax.top>
2025-06-10 16:12:25 +0800

    Added access log

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ export default {
       const key = matchRead[1];
       // Increment view count atomically
       await env.BLOG_VIEW_COUNT.put(key, ((parseInt(await env.BLOG_VIEW_COUNT.get(key)) || 0) + 1).toString());
+
+      // Log the view, with page, user agent, user IP and timestamp
+      console.log({
+        page: key,
+        userAgent: request.headers.get('User-Agent'),
+        userIP: request.headers.get('CF-Connecting-IP'),
+        userLocation: request.cf.country + '-' + request.cf.city,
+        timestamp: new Date().toISOString(),
+      });
+
       // 1x1 transparent PNG
       const png = Uint8Array.from([
         137,80,78,71,13,10,26,10,0,0,0,13,73,72,68,82,0,0,0,1,0,0,0,1,8,6,0,0,0,31,21,196,137,
