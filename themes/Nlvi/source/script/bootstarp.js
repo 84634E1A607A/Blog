89370bc09f71117baf08e1b1e661af8ef0ad7d5d
Ajax Dong <i@aajax.top>
2026-02-15 23:10:26 +0800

    Added blog theme as monorepo

---
+++ themes/Nlvi/source/script/bootstarp.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.body.addEventListener('touchstart', function () {});
+  var app = new Nlvi(nlviconfig);
+  app.bootstarp();
+});
